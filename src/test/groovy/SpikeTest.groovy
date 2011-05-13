a9fb1e0f09119a02b4aeaad307eebbac66d36de0
Jacky Chan <linux_china@hotmail.com>
2011-05-13 19:00:10 +0800

    spike test case

---
+++ src/test/groovy/SpikeTest.groovy
@@ -0,0 +1,11 @@
+/**
+ * spike test case
+ */
+class SpikeTest extends GroovyTestCase {
+    /**
+     * test method for spike
+     */
+    public void testSpike() {
+        println("Hello Beaver!");
+    }
+}
