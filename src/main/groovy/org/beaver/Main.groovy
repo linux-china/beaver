7c2ec768381e70e4bdfb3451c11f20271365dcab
Jacky Chan <linux_china@hotmail.com>
2011-05-13 19:04:06 +0800

    beaver main class

---
+++ src/main/groovy/org/beaver/Main.groovy
@@ -0,0 +1,17 @@
+package org.beaver
+
+/**
+ * beaver main class
+ *
+ * @author linux_china
+ */
+class Main {
+    /**
+     * entrance to run beaver
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+        println("Hello Beaver!")
+    }
+}
