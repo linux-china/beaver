1cf824e89f8f0673c97780f87f6d405f15816dd4
linux_china <libing.chen@gmail.com>
2011-05-13 19:51:47 +0800

    beaver wrapper main class for command line

---
+++ src/main/groovy/org/beaver/BeaverWrapperMain.groovy
@@ -0,0 +1,12 @@
+package org.beaver
+
+/**
+ * beaver wrapper main class for command line
+ *
+ * @author linux_china
+ */
+class BeaverWrapperMain {
+  public static void main(String[] args) {
+    println("Hello Beaver!")
+  }
+}
