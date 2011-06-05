2c867d626239c887644e2b1b6752a4e054320809
linux_china <libing.chen@gmail.com>
2011-06-05 13:16:27 +0800

    hello spec

---
+++ src/test/groovy/HelloSpec.groovy
@@ -0,0 +1,15 @@
+import spock.lang.Specification
+/**
+ * hello spec
+ *
+ * @author linux_china
+ */
+class HelloSpec extends Specification {
+  /**
+   * spike test
+   */
+  public void testSpike() {
+    expect:
+      1==1
+  }
+}
