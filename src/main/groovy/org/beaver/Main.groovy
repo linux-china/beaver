7cd522e0a66e1df2dc02a77e75751e73acbb872d
Jacky Chan <linux_china@hotmail.com>
2011-08-14 16:14:51 +0800

    添加cli支持

--- src/main/groovy/org/beaver/Main.groovy
+++ src/main/groovy/org/beaver/Main.groovy
@@ -12,6 +12,21 @@ class Main {
      * @param args args
      */
     public static void main(String[] args) {
-        println("Hello Beaver!")
+        CliBuilder cl = new CliBuilder(usage: 'beaver env_name')
+        cl.h(longOpt: 'help', '显示帮助信息')
+        cl.v(longOpt: 'version', '显示版本号')
+        cl.i(longOpt: 'install', '安装一个新的环境配置')
+        def opt = cl.parse(args)
+        if (!opt) {
+            println "\n不合法的命令行，退出..."
+        } else if (opt.h) {
+            cl.usage()
+        } else if (opt.i) {
+            println "install env: ${opt.i}"
+        } else if (opt.v) {
+            println("Beaver 1.0.0")
+        } else {
+            String envName = opt.arguments()[0];
+        }
     }
 }
