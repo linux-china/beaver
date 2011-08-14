338364a8ff407a9ffc238ee244a15b4672d2945d
Jacky Chan <linux_china@hotmail.com>
2011-08-14 17:08:51 +0800

    添加list和切换机制

--- src/main/groovy/org/beaver/Main.groovy
+++ src/main/groovy/org/beaver/Main.groovy
@@ -12,6 +12,8 @@ class Main {
      * @param args args
      */
     public static void main(String[] args) {
+        //repository
+        Repository.init(new File(System.getProperty("user.home"), ".beaver"))
         CliBuilder cl = new CliBuilder(usage: 'beaver env_name')
         cl.h(longOpt: 'help', '显示帮助信息')
         cl.v(longOpt: 'version', '显示版本号')
@@ -25,8 +27,38 @@ class Main {
             println "install env: ${opt.i}"
         } else if (opt.v) {
             println("Beaver 1.0.0")
+        } else if (opt.arguments().isEmpty()) {
+            cl.usage()
         } else {
-            String envName = opt.arguments()[0];
+            String command = opt.arguments()[0];
+            switch (command) {
+                case 'list': printItems(); break;
+                default: switchEnvItem(command);
+
+            }
         }
     }
+    /**
+     * 切换env item
+     * @param name
+     */
+    public static void switchEnvItem(String name) {
+        def envItem = Repository.instance.findEnvItemByName(name);
+        if (envItem == null) {
+            println("${name} 不存在!")
+        } else {
+            Repository.instance.switchItem(name);
+            println("切换成功!")
+        }
+    }
+    /**
+     * 打印env item信息
+     * @param repo repository
+     */
+    public static void printItems() {
+        Repository.instance.items.items.each {envItem ->
+            println "${envItem.name}"
+        }
+    }
+
 }
