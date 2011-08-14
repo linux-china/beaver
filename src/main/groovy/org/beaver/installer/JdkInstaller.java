ef2616410d4ddd0d5a15797779169597cf17db31
Jacky Chan <linux_china@hotmail.com>
2011-08-14 18:01:51 +0800

    minor update

--- src/main/groovy/org/beaver/installer/JdkInstaller.java
+++ src/main/groovy/org/beaver/installer/JdkInstaller.java
@@ -20,6 +20,6 @@ public class JdkInstaller extends Installer {
      */
     @Override
     public void execute() {
-        //To change body of implemented methods use File | Settings | File Templates.
+
     }
 }
