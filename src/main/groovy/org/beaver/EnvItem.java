d92e7fc58aba5df32abc7ed6fca3d89eb312b69c
Jacky Chan <linux_china@hotmail.com>
2011-08-14 17:06:58 +0800

    env item

---
+++ src/main/groovy/org/beaver/EnvItem.java
@@ -0,0 +1,17 @@
+package org.beaver;
+
+/**
+ * env item
+ *
+ * @author linux_china
+ */
+public class EnvItem {
+    /**
+     * 名称
+     */
+    String name;
+    /**
+     * 安装的目录
+     */
+    String targetDir;
+}
