2ccaa5c66b7ae1a605e9b5414fbaa0e50e3e4971
Jacky Chan <linux_china@hotmail.com>
2011-08-14 17:56:56 +0800

    完善数据

---
+++ src/main/groovy/org/beaver/installer/JdkInstaller.java
@@ -0,0 +1,17 @@
+package org.beaver.installer;
+
+/**
+ * jdk installer
+ *
+ * @author linux_china
+ */
+public class JdkInstaller {
+    /**
+     * 版本号
+     */
+    String version;
+    /**
+     * 架构，32位或者64位
+     */
+    String arch;
+}
